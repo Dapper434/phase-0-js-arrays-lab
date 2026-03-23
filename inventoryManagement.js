6eccf297f54f6e08832dea33254de687aa4c2a06
Pipsy <gpflaslik7112@gmail.com>
2026-03-23 23:38:43 +0300

    initia commit of js files

--- inventoryManagement.js
+++ inventoryManagement.js
@@ -1,7 +1,31 @@
 // Write your code here
+const products = ["Laptop", "Phone", "Headphones", "Monitor"];
 
 
+console.log(products);
 
+
+function logFirstProduct() {
+  
+  console.log("The first product in inventory is:", products[0]);
+}
+
+function addProduct(productName) {
+  products.push(productName);
+  console.log(productName + " was added to the inventory.");
+}
+
+function updateProductName(position, newName) {
+  products[position] = newName;
+  console.log("Product at index " + position + " was updated to " + newName + ".");
+}
+
+function removeLastProduct() {
+  let removedItem = products.pop();
+  console.log(removedItem + " was removed from the inventory.");
+}
+
+logFirstProduct();
 // Export the necessary parts for testing
 module.exports = {
   logFirstProduct: typeof logFirstProduct !== 'undefined' ? logFirstProduct : undefined,
